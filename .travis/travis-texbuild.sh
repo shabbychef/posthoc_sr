d2b475efaa00ce8e747c66a92b83c5ca3cf11038
Steven Pav <shabbychef@gmail.com>
2019-11-13 22:15:33 -0800

    ignore unmatch?
    still does not build

--- .travis/travis-texbuild.sh
+++ .travis/travis-texbuild.sh
@@ -30,7 +30,7 @@ setup_git() {
   fi
   git checkout --orphan "travis-$TRAVIS_BUILD_NUMBER"
   git reset
-  git rm -f --cached $(git ls-files)
+  git rm -f --ignore-unmatch --cached $(git ls-files)
 }
 
 commit_pdfs() {
